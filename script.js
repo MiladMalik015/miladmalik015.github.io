2271cb787b41e59de4289d5f0b38061e18d7cb4c
/Milad Malik/ <miladdrop015@hotmail.com>
2026-02-28 00:21:14 +0430

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const canvas = document.getElementById("bg");
+/*const canvas = document.getElementById("bg");
 const ctx = canvas.getContext("2d");
 
 canvas.width = window.innerWidth;
@@ -51,3 +51,4 @@ function draw() {
 }
 
 draw();
+*/
